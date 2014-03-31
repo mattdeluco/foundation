48be23443bd881cc37115571d56f836307640e75
Matt DeLuco <matt@deluco.net>
2014-03-31 10:30:56 -0400

    Completed unit tests for backend user model.

--- app/users/tests/UserModelTest.js
+++ app/users/tests/UserModelTest.js
@@ -11,106 +11,106 @@ var should = require('should'),
 var user, user2;
 
 //The tests
-describe('<Unit Test>', function() {
-    describe('Model User:', function() {
-        before(function(done) {
-            user = new User({
-                name: 'Full name',
-                email: 'test@example.com',
-                username: 'user',
-                password: 'password',
-                provider: 'local'
-            });
-            user2 = new User({
-                name: 'Full name',
-                email: user.email,
-                username: 'user',
-                password: 'password',
-                provider: 'local'
-            });
+describe('User Model', function() {
 
-            done();
+    before(function(done) {
+        user = new User({
+            name: 'Full name',
+            email: 'test@example.com',
+            username: 'user',
+            password: 'password',
+            provider: 'local'
+        });
+        user2 = new User({
+            name: 'Full name',
+            email: user.email,
+            username: 'user',
+            password: 'password',
+            provider: 'local'
         });
 
-        describe('Method Save', function() {
-            it('should begin with no users', function(done) {
-                User.find({}, function(err, users) {
-                    if (err) return done(err);
-                    users.should.have.length(0);
-                    done();
-                });
-            });
+        done();
+    });
 
-            it('should save without error', function(done) {
-                user.save(done);
-            });
-
-            it('should fail to save a new user with an existing email address', function(done) {
-                user.save();
-                return user2.save(function(err) {
-                    should.exist(err);
-                    done();
-                });
-            });
-
-            it('should save a new user with a different email address', function(done) {
-                user.save();
-                var user3 = new User(user2);
-                user3.email = 'test3@example.com';
-                user3.save(done);
-            });
-
-            it('should error on empty name', function(done) {
-                var userx = new User(user);
-                userx.name = '';
-                return userx.save(function(err) {
-                    should.exist(err);
-                    done();
-                });
-            });
-
-            it('should error on empty email', function(done) {
-                var userx = new User(user);
-                userx.email = '';
-                return userx.save(function(err) {
-                    should.exist(err);
-                    done();
-                });
-            });
-
-            it('should error on empty password', function(done) {
-                var userx = new User(user);
-                userx.password = '';
-                return userx.save(function(err) {
-                    should.exist(err);
-                    done();
-                });
+    describe('Method Save', function() {
+        it('should begin with no users', function(done) {
+            User.find({}, function(err, users) {
+                if (err) return done(err);
+                users.should.have.length(0);
+                done();
             });
         });
 
-        describe('Password', function() {
-            it('should store the password as a bcrypt hash', function(done) {
-                user.save();
-                User.findOne({_id: user.id}, '+hashed_password', function(err, user) {
-                    if (err) return done(err);
-                    user.hashed_password.should.startWith('$2a$10$');
-                    done();
-                });
-            });
+        it('should save without error', function(done) {
+            user.save(done);
+        });
 
-            it('should not return hashed_password in a query', function(done) {
-                user.save();
-                User.findOne({_id: user.id}, function(err, user) {
-                    if (err) return done(err);
-                    user.should.not.have.property('hashed_password');
-                    done();
-                });
+        it('should fail to save a new user with an existing email address', function(done) {
+            user.save();
+            return user2.save(function(err) {
+                should.exist(err);
+                done();
             });
         });
 
-        after(function(done) {
-            User.remove().exec();
-            done();
+        it('should save a new user with a different email address', function(done) {
+            user.save();
+            var user3 = new User(user2);
+            user3.email = 'test3@example.com';
+            user3.save(done);
+        });
+
+        it('should error on empty name', function(done) {
+            var userx = new User(user);
+            userx.name = '';
+            return userx.save(function(err) {
+                should.exist(err);
+                done();
+            });
+        });
+
+        it('should error on empty email', function(done) {
+            var userx = new User(user);
+            userx.email = '';
+            return userx.save(function(err) {
+                should.exist(err);
+                done();
+            });
+        });
+
+        it('should error on empty password', function(done) {
+            var userx = new User(user);
+            userx.password = '';
+            return userx.save(function(err) {
+                should.exist(err);
+                done();
+            });
         });
     });
+
+    describe('Password', function() {
+        it('should store the password as a bcrypt hash', function(done) {
+            user.save();
+            User.findOne({_id: user.id}, '+hashed_password', function(err, user) {
+                if (err) return done(err);
+                user.hashed_password.should.startWith('$2a$10$');
+                done();
+            });
+        });
+
+        it('should not return hashed_password in a query', function(done) {
+            user.save();
+            User.findOne({_id: user.id}, function(err, user) {
+                if (err) return done(err);
+                user.should.not.have.property('hashed_password');
+                done();
+            });
+        });
+    });
+
+    after(function(done) {
+        User.remove().exec();
+        done();
+    });
+
 });
