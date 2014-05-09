5ff8393ab05d1ed2b27d03692f33f4b06980d293
Matt DeLuco <matt@deluco.net>
2014-05-09 11:58:31 -0400

    Moved snInputMatch directive to more central location.

--- public/client/directives.js
+++ public/client/directives.js
@@ -1,1 +1,25 @@
+/**
+ * Created by mdeluco on 2014-04-27.
+ */
+
 'use strict';
+
+angular.module('mean.users').directive(
+    'snInputMatch',
+    function() {
+        return {
+            restrict: 'A',
+            replace: false,
+            require: 'ngModel',
+            link: function (scope, elem, attrs, ctrl) {
+                var checker = function () {
+                    return scope.$eval(attrs.ngModel) == scope.$eval(attrs.snInputMatch);
+                };
+
+                scope.$watch(checker, function (value) {
+                    ctrl.$setValidity('unique', value);
+                });
+            }
+        }
+    }
+);
