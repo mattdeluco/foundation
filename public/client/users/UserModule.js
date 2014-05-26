09db7c0458ba46bc67af6e1dd44fd9f6b812b94a
Matt DeLuco <matt@deluco.net>
2014-05-26 13:07:02 -0400

    Moved User configuration into module file.

--- public/client/users/UserModule.js
+++ public/client/users/UserModule.js
@@ -7,4 +7,25 @@ angular.module('mean.users', [
     'mean.system',
     'ngResource',
     'ui.router'
+]).config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+        $stateProvider.
+            state('signup', {
+                url: '/signup',
+                templateUrl: '/client/users/views/create.html',
+                controller: 'UserCtrl'
+            }).
+            state('me', {
+                url: '/me',
+                templateUrl: '/client/users/views/view.html',
+                controller: 'UserCtrl'
+            }).
+            state('edit', {
+                url: '/me/edit',
+                templateUrl: '/client/users/views/edit.html',
+                controller: 'UserCtrl'
+            });
+    }
 ]);
